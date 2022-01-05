2d6a3d917a78cb5dffb1e3fad50a5557e07e9a10
Brayam Martínez Perdomo <92887752+Biko87@users.noreply.github.com>
2022-01-05 15:32:23 -0500

    Update enviar.php

--- send_form/enviar.php
+++ send_form/enviar.php
@@ -45,7 +45,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'brayammartinezperdomo@gmail.com';                     //SMTP username
-    $mail->Password   = 'drqqbd77ss*16';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
